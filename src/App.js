87219066553cdac313b4e2af89b20474f4ab672e
Chrisringler <chrisringler@outlook.com.ar>
2023-04-27 16:55:40 -0300

    Categorias y Home principal components

--- src/App.js
+++ src/App.js
@@ -3,9 +3,11 @@ import './App.css';
 import Products from './components/Products/Products';
 import Register from './pages/register/Register';
 import Login from './pages/login/Login';
+import HomePrincipal from './pages/HomePrincipal/HomePrincipal';
 function App() {
   return (
-    <div className="App">
+    <div className="App">     
+        <Route exact path="/" component={HomePrincipal} />
         <Route exact path="/products" component={Products } />
         <Route exact path="/registrar-usuario" component={Register} />
         <Route exact path="/log-in" component={Login} />
